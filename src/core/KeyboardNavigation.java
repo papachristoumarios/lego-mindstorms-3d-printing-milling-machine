5a2055b9b3300df99d8169e9cb0c5d99644b81a3
MaR1oC <mrmarios97@gmail.com>
2013-05-06 20:50:15 +0300

    created gui navigator

--- src/core/KeyboardNavigation.java
+++ src/core/KeyboardNavigation.java
@@ -1,26 +1,79 @@
 package core;
 
+import javax.xml.bind.Marshaller.Listener;
+
 import lejos.addon.keyboard.KeyEvent;
 import lejos.addon.keyboard.KeyListener;
+import lejos.nxt.Motor;
 
-public class KeyboardNavigation implements KeyListener {
+/*
+* KeyboardNavigation
+*/
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+
+public class KeyboardNavigation {
+    
+	public static void main (String arg) {
+		while(true) {System.out.println("ha");}
+		
+		
 		
 	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		//if (e.VK_UP)
+	
+	
+    
+    private static final KeyListener listener = new KeyListener() {
 		
-	}
+	
+    
+    /** Handle the key typed event from the text field. */
+    public void keyTyped(KeyEvent e) {
+        displayInfo(e);
+    }
+    
+    /** Handle the key pressed event from the text field. */
+    public void keyPressed(KeyEvent e) {
+    	 displayInfo(e);
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			Motor.A.backward();
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
+			Motor.A.forward();
+		} else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			System.exit(0);
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			Motor.B.forward(); }
+       
+    }
+    
+    /** Handle the key released event from the text field. */
+    public void keyReleased(KeyEvent e) {
+        displayInfo(e);
+    }
+    
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+    private void displayInfo(KeyEvent e){
+    
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			System.out.println("X axis : +\n");
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
+			System.out.println("X axis: -\n");		
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			System.out.println("Y axis: +\n"); 
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			System.out.println("Y axis: -\n");
+		} else if (e.getKeyCode() == KeyEvent.VK_NUMPAD4) {
+			System.out.println("Z axis: +\n"); 
+		} else if (e.getKeyCode() == KeyEvent.VK_NUMPAD6) {
+			System.out.println("Z axis: -\n");
+		} else { System.out.println("Invalid key-press\n");
+		}
 		
-	}
-
+    	
+    }    	
+    	
+    	
+    };
 }
+
+    
+    
---
+++ src/core/ui/GUINavigation.java
@@ -0,0 +1,187 @@
+package core.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import lejos.nxt.Motor;
+import lejos.nxt.NXT;
+import lejos.nxt.Sound;
+import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.remote.NXTComm;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+
+public class GUINavigation {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUINavigation window = new GUINavigation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUINavigation() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[][][][][][][][][]", "[][][][][][][][][][]"));
+		
+		JButton beepButton = new JButton("Beep!");
+		beepButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Sound.beep();
+			}
+		});
+		frame.getContentPane().add(beepButton, "cell 1 0");
+		
+		JCheckBox drillToggle = new JCheckBox("Drill Toggle");
+		frame.getContentPane().add(drillToggle, "cell 3 0 2 1");
+		
+		JButton zstop = new JButton("Stop!");
+		zstop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.C.stop();
+				
+			}
+		});
+		frame.getContentPane().add(zstop, "cell 6 5");
+		
+		final JSlider slider = new JSlider();
+		slider.setToolTipText("Slider has a min of 10 and a max of 150");
+		slider.setValue(10);
+		slider.setMaximum(150);
+		frame.getContentPane().add(slider, "cell 1 8 8 1");
+		
+		JButton xstop = new JButton("Stop!");
+		xstop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.A.stop();
+			}
+		});
+		
+		JLabel connectionLabel = new JLabel("Connection : No");
+		frame.getContentPane().add(connectionLabel, "cell 7 0 2 1");
+		
+		JLabel zlabel = new JLabel("Z");
+		frame.getContentPane().add(zlabel, "cell 6 2,alignx center");
+		
+		JButton zplus = new JButton("+");
+		zplus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.C.setSpeed(slider.getValue());
+				Motor.C.forward();
+				
+			}
+		});
+		frame.getContentPane().add(zplus, "cell 6 3,growx");
+		
+		JButton zminus = new JButton("-");
+		zminus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.C.setSpeed(slider.getValue());
+				Motor.C.backward();
+			}
+		});
+		frame.getContentPane().add(zminus, "cell 6 4,growx");
+		frame.getContentPane().add(xstop, "cell 1 5");
+		
+		JButton btnNewButton = new JButton("Stop!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.B.stop();
+			}
+		});
+		frame.getContentPane().add(btnNewButton, "cell 3 5,growx");
+		
+		
+		
+		JLabel xlabel = new JLabel("X");
+		frame.getContentPane().add(xlabel, "cell 1 2,alignx center");
+		
+		JLabel ylabel = new JLabel("Y");
+		frame.getContentPane().add(ylabel, "cell 3 2,alignx center");
+		
+		JButton xplus = new JButton("+");
+		xplus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Motor.A.setSpeed(slider.getValue());
+				Motor.A.forward();
+			}
+		});
+		frame.getContentPane().add(xplus, "cell 1 3,growx");
+		
+		JButton xminus = new JButton("-");
+		xminus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.A.setSpeed(slider.getValue());
+				Motor.A.backward();
+				
+			}
+		});
+		
+		JButton yplus = new JButton("+");
+		yplus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Motor.B.setSpeed(slider.getValue());
+				Motor.B.forward();
+			}
+		});
+		frame.getContentPane().add(yplus, "cell 3 3,growx");
+		frame.getContentPane().add(xminus, "cell 1 4,growx");
+		
+		JButton yminus = new JButton("-");
+		yminus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Motor.B.setSpeed(slider.getValue());
+				Motor.B.backward();
+			}
+		});
+		frame.getContentPane().add(yminus, "cell 3 4,growx");
+		
+		JLabel lblNewLabel = new JLabel("Velocity");
+		frame.getContentPane().add(lblNewLabel, "cell 1 7");
+		
+		JButton shutdown = new JButton("Shutdown NXT");
+		shutdown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NXT.exit(0);
+			}
+		});
+		frame.getContentPane().add(shutdown, "cell 1 9");
+		
+
+
+	}
+	
+	
+
+}
